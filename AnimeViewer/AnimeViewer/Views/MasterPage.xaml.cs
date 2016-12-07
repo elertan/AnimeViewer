b85fa3194914d9ee186d8a272ed4e4309db1f4cf
elertan <denkievits@gmail.com>
2016-12-07 09:32:27 +0100

    Added menu icon for ios and fixed colors to white

--- AnimeViewer/AnimeViewer/Views/MasterPage.xaml.cs
+++ AnimeViewer/AnimeViewer/Views/MasterPage.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace AnimeViewer.Views
 {
@@ -12,6 +6,8 @@ namespace AnimeViewer.Views
     {
         public MasterPage()
         {
+            if (Device.OS == TargetPlatform.iOS)
+                Icon = "menu.png";
             InitializeComponent();
         }
     }
