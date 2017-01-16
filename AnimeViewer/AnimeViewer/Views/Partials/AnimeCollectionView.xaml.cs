ddc80b3cb111d9f8e18834abae6f5776653ce023
elertan <denkievits@gmail.com>
2017-01-16 09:50:23 +0100

    Added labels to indicate popular and updated animes

---
+++ AnimeViewer/AnimeViewer/Views/Partials/AnimeCollectionView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using AnimeViewer.Models;
+using Xamarin.Forms;
+
+namespace AnimeViewer.Views.Partials
+{
+    public partial class AnimeCollectionView : ContentView
+    {
+        public static readonly BindableProperty AnimesProperty = BindableProperty.Create(nameof(Animes), typeof(IEnumerable), null);
+
+        public AnimeCollectionView()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        public IEnumerable Animes
+        {
+            get { return (IEnumerable) GetValue(AnimesProperty); }
+            set { SetValue(AnimesProperty, value); }
+        }
+
+        private async void FlowListView_OnFlowItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            // Get the corrosponding anime that was tapped
+            var anime = (Anime) e.Item;
+            // Show the anime page for that anime
+            await Navigation.PushAsync(new AnimePage(anime));
+        }
+    }
+}
